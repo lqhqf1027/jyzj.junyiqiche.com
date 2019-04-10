fe381e7f32b39b5e83d1113fa5f19800776389fe
Lance-93 <glen92093@gmail.com>
2019-04-10 18:28:36 +0800

    xiufu

--- application/admin/model/Schemecategory.php
+++ application/admin/model/Schemecategory.php
@@ -8,24 +8,24 @@ class Schemecategory extends Model
 {
     // 表名
     protected $name = 'scheme_category';
-    
-    // 自动写入时间戳字段
+
+    // 自动写入时间戳字段222
     protected $autoWriteTimestamp = 'int';
 
     // 定义时间戳字段名
     protected $createTime = 'createtime';
     protected $updateTime = 'updatetime';
-    
+
     // 追加属性
     protected $append = [
 
     ];
-    
+
     public function cities()
     {
         return $this->belongsTo('Cities', 'city_id', 'id', [], 'LEFT')->setEagerlyType(0);
     }
-    
+
 
 
 
