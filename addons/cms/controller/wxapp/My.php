8c94e9f0191fba6f1146c2b276893bb47022fe20
Lance-93 <glen92093@gmail.com>
2019-04-16 18:32:20 +0800

    cms

---
+++ addons/cms/controller/wxapp/My.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace addons\cms\controller\wxapp;
+use think\Db;
+use Endroid\QrCode\QrCode;
+use fast\Random;
+use think\Exception;
+/**
+ * 我的
+ */
+class My extends Base
+{
+    protected $noNeedLogin = ['aboutus'];
+
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
+
+    /**
+     * 生成二维码
+     * @throws \Endroid\QrCode\Exceptions\ImageTypeInvalidException
+     */
+    public function setQrcode()
+    {
+        $user_id = $this->request->post('user_id');
+        if (!(int)$user_id) $this->error('参数错误');
+        $time = date('YmdHis');
+        $qrCode = new QrCode();
+        $qrCode->setText($_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/?user_id=' . $user_id)
+            ->setSize(150)
+            ->setPadding(10)
+            ->setErrorCorrection('high')
+            ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
+            ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
+            ->setLabel(' ')
+            ->setLabelFontSize(10)
+            ->setImageType(\Endroid\QrCode\QrCode::IMAGE_TYPE_PNG);
+        $fileName = DS . 'uploads' . DS . 'qrcode' . DS . $time . '_' . $user_id . '.png';
+        $qrCode->save(ROOT_PATH . 'public' . $fileName);
+        if ($qrCode) {
+            User::update(['id' => $user_id, 'invitation_code_img' => $fileName]) ? $this->success('创建成功', $fileName) : $this->error('创建失败');
+        }
+        $this->error('未知错误');
+    }
+}
