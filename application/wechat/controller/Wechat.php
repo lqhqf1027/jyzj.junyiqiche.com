712795599908fd2fabccc8fc3f558aec502eb6df
Lance-93 <glen92093@gmail.com>
2019-04-25 18:29:51 +0800

    修改页面

--- application/wechat/controller/Wechat.php
+++ application/wechat/controller/Wechat.php
@@ -65,7 +65,7 @@ class Wechat extends Controller
             $token = $this->Wxapis->getWxtoken()['access_token'];
             $r = gets("https://api.weixin.qq.com/cgi-bin/user/info?access_token={$token}&openid=" . $userInfo['openid']);
             if (!$r['subscribe']) {
-                $this->alert('请先关注公众号，点击logo头像即可关注！', 'jump', 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyODAyNjE3NA==&scene=126&bizpsid=0&subscene=0#wechat_redirect');
+                alert('请先关注公众号，点击logo头像即可关注！', 'jump', 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyODAyNjE3NA==&scene=126&bizpsid=0&subscene=0#wechat_redirect');
 //                header('Location:https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyODAyNjE3NA==&scene=126&bizpsid=0&subscene=0#wechat_redirect');
             }
             $res = WxPublicUser::create($userInfo)->getData();
@@ -91,44 +91,6 @@ class Wechat extends Controller
         }
     }
 
-    /**
-     * JS提示跳转
-     * @param  $tip  弹窗口提示信息(为空没有提示)
-     * @param  $type 设置类型 close = 关闭 ，back=返回 ，refresh=提示重载，jump提示并跳转url
-     * @param  $url  跳转url
-     */
-    public function alert($tip = "", $type = "", $url = "")
-    {
-        $js = "<script>";
-        if ($tip)
-            $js .= "alert('" . $tip . "');";
-        switch ($type) {
-            case "close" : //关闭页面
-                $js .= "window.close();";
-                break;
-            case "back" : //返回
-                $js .= "history.back(-1);";
-                break;
-            case "refresh" : //刷新
-                $js .= "parent.location.reload();";
-                break;
-            case "top" : //框架退出
-                if ($url)
-                    $js .= "top.location.href='" . $url . "';";
-                break;
-            case "jump" : //跳转
-                if ($url)
-                    $js .= "window.location.href='" . $url . "';";
-                break;
-            default :
-                break;
-        }
-        $js .= "</script>";
-        echo $js;
-        if ($type) {
-            exit();
-        }
-    }
 
     // 用于请求微信接口获取数据
     public function get_by_curl($url, $post = false)
