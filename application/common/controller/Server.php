a6273ab27e8f83dce7969938732242bfe0153dda
Lance-93 <glen92093@gmail.com>
2019-04-29 18:33:50 +0800

    easywechat

---
+++ application/common/controller/Server.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: glen9
+ * Date: 2019/4/29
+ * Time: 17:20
+ */
+
+namespace app\common\controller;
+
+
+use EasyWeChat\Payment\Order;
+use think\Controller;
+
+class Server extends Controller
+{
+    public function a(){
+
+    }
+
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit93719bb1aa6fa692188d6b4db5c12d54
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit93719bb1aa6fa692188d6b4db5c12d54', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
