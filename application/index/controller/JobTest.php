0205e094c04d4a5ba6b61af25dbb0f827b2c73d3
Lance-93 <glen92093@gmail.com>
2019-05-21 14:56:19 +0800

    think-queue

--- application/index/controller/JobTest.php
+++ application/index/controller/JobTest.php
@@ -17,20 +17,21 @@ class JobTest extends Controller
 {
     public function actionWithHelloJob()
     {
-               // 1.当前任务将由哪个类来负责处理。
+        // 1.当前任务将由哪个类来负责处理。
         //   当轮到该任务时，系统将生成一个该类的实例，并调用其 fire 方法
         $jobHandlerClassName = 'app\index\job\Hello';
 
         // 2.当前任务归属的队列名称，如果为新队列，会自动创建
-        $jobQueueName = "helloJobQueue";
+        $jobQueueName = "helloJobQueue1";
 
         // 3.当前任务所需的业务数据 . 不能为 resource 类型，其他类型最终将转化为json形式的字符串
         //   ( jobData 为对象时，存储其public属性的键值对 )
-        $jobData = ['order_no' => rand(100000, 999999)];
-        $this->add($jobData['order_no']);
+//        $jobData       	  = [ 'ts' => time(), 'bizId' => uniqid() , 'a' => 1 ] ;
+        $orderNo = array('order_no' => rand(222, 999));
+        $jobData = $this->add($orderNo['order_no']);
         // 4.将该任务推送到消息队列，等待对应的消费者去执行
-        $isPushed = Queue::push($jobHandlerClassName, $jobData, $jobQueueName);
-//
+        $isPushed = Queue::push($jobHandlerClassName, $orderNo, $jobQueueName);
+
         // database 驱动时，返回值为 1|false  ;   redis 驱动时，返回值为 随机字符串|false
         if ($isPushed !== false) {
             echo date('Y-m-d H:i:s') . " a new Hello Job is Pushed to the MQ" . "<br>";
--- application/index/job/Hello.php
+++ application/index/job/Hello.php
@@ -7,41 +7,73 @@
  */
 
 namespace app\index\job;
+
+use think\Db;
 use think\queue\Job;
 
 class Hello
 {
     /**
      * fire方法是消息队列默认调用的方法
-     * @param Job            $job      当前的任务对象
-     * @param array|mixed    $data     发布任务时自定义的数据
+     * @param Job $job 当前的任务对象
+     * @param array|mixed $data 发布任务时自定义的数据
      */
-    public function fire(Job $job,$data)
+    public function fire(Job $job, $data)
     {
-        print("<info>Hello Job has been done and deleted" . "</info>\n");
-        //....这里执行具体的任务
-        $data = json_decode($data, true);
-        if ($this->jobDone($data)) {
+        // 有些消息在到达消费者时,可能已经不再需要执行了
+        $isJobStillNeedToBeDone = $this->checkDatabaseToSeeIfJobNeedToBeDone($data);
+        if (!$isJobStillNeedToBeDone) {
+            $job->delete();
+            return;
+        }
+
+        $isJobDone = $this->doHelloJob($data);
+
+        if ($isJobDone) {
+            // 如果任务执行成功， 记得删除任务
             $job->delete();
             print("<info>Hello Job has been done and deleted" . "</info>\n");
         } else {
-            $job->release(3); //$delay为延迟时间
+            if ($job->attempts() > 3) {
+                //通过这个方法可以检查这个任务已经重试了几次了
+                print("<warn>Hello Job has been retried more than 3 times!" . "</warn>\n");
+
+                $job->delete();
+
+                // 也可以重新发布这个任务
+                //print("<info>Hello Job will be availabe again after 2s."."</info>\n");
+                //$job->release(2); //$delay为延迟时间，表示该任务延迟2秒后再执行
+            }
         }
-        if ($job->attempts() > 3) {
-            //通过这个方法可以检查这个任务已经重试了几次了
-        }
-        //如果任务执行成功后 记得删除任务，不然这个任务会重复执行，直到达到最大重试次数后失败后，执行failed方法
-        // $job->delete();
-        // 也可以重新发布这个任务
-        // $job->release($delay); //$delay为延迟时间
     }
 
     public function jobDone($data)
     {
-        print("<info>Job is Done status!" . "</info> \n");
-//        return 1;
-
-//        return Db::name('tp5_test')->where(['order_no' => $data['order_no']])->update(['status' => 1]);
+        /**
+         * 有些消息在到达消费者时,可能已经不再需要执行了
+         * @param array|mixed $data 发布任务时自定义的数据
+         * @return boolean                 任务执行的结果
+         */
     }
+
+    private function checkDatabaseToSeeIfJobNeedToBeDone($data)
+    {
+        return true;
+    }
+
+
+    /**
+     * 根据消息中的数据进行实际的业务处理...
+     */
+    private function doHelloJob($data)
+    {
+        Db::name('tp5_test')->where(['order_no'=>$data['order_no']])->update(['status'=>1]);
+        print("<info>Hello Job Started. job Data is: " . var_export($data, true) . "</info> \n");
+        print("<info>Hello Job is Fired at " . date('Y-m-d H:i:s') . "</info> \n");
+        print("<info>Hello Job is Done!" . "</info> \n");
+
+        return true;
+    }
+
 
 }
